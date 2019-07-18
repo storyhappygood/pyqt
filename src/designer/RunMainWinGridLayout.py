b3d4f8d7e81593f6c7cc9e153fb265dee53ac940
storyhappygood <1558276720@qq.com>
2019-07-18 22:12:38 +0800

    formlayout

---
+++ src/designer/RunMainWinGridLayout.py
@@ -0,0 +1,18 @@
+import sys
+import MainWinGridLayout
+
+from PyQt5.QtWidgets import QApplication,QMainWindow
+
+if __name__=='__main__':
+    # 创建QApplication的实例
+    app = QApplication(sys.argv)
+    # 创建一个窗口
+    mainWindow = QMainWindow()
+    # 创建一个MainWinHorizontalLayout对象
+    ui = MainWinGridLayout.Ui_MainWindow()
+    # 向主窗口添加控件
+    ui.setupUi(mainWindow)
+    # 窗口显示
+    mainWindow.show()
+    # 安全退出
+    sys.exit(app.exec_())
